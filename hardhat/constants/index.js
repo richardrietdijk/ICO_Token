dd9ecd2e2203aae5ae813f3d2a8cee9f553abd31
Richard Rietdijk <richardrietdijk@Richards-MacBook-Pro.local>
2023-05-26 11:35:12 -0500

    implement hardhat contracts

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0xF1fd385FBe7AC88eC0B13AF2c7b86707Dc25EcA5";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
